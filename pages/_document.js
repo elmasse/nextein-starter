05fd3718f2e18172bdf7a12d952ccea718fe80b1
Max Fierro <elmasse@gmail.com>
2018-11-21 00:02:35 -0300

    Added viewport meta

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <link type="text/css" rel="stylesheet" href="/static/stylesheet.css" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main />
