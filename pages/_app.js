f33b73658c54338330858b6eccdf9d12935ebbca
Max Fierro <elmasse@gmail.com>
2020-05-04 10:36:37 -0300

    Update _app.js. Remove unused static method.

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,7 @@ import React from 'react'
 import App from 'next/app'
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {}
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
 
   render() {
     const { Component, pageProps } = this.props
