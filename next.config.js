a7eb47b46fc31ef7e603b8c55ef2d6f863809bc1
Max Fierro <elmasse@gmail.com>
2022-08-10 16:49:41 -0300

    update dependencies.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer(
+module.exports =
   withNextein({})
-)
+
