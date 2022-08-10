63e4334fdab867dc2b43f2ca2504636dc7c8d232
Max Fierro <elmasse@gmail.com>
2022-08-10 16:51:37 -0300

    restore bundle analyzer

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports =
-  withNextein({})
-
+module.exports = withBundleAnalyzer(
+  withNextein({
+    experimental: {
+    }
+  })
+)
